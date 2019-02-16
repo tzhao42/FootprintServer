0b34d9e68b3c2c67c456df95fc88dbc6a342512d
Ubuntu <ubuntu@ip-172-31-46-69.us-east-2.compute.internal>
2019-02-16 11:15:29 +0000

    stuff

--- fp_server/fp_client/views.py
+++ fp_server/fp_client/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.db import IntegrityError
 from django.contrib.auth.hashers import check_password, make_password
-#from . import models
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Users
 from .models import Trips
 
 def index(request):
     return HttpResponse('heck')
 
+@csrf_exempt
 def signup(request):
 	email_in = request.POST.get('email')
 	password_in = request.POST.get('password')
@@ -21,6 +23,7 @@ def signup(request):
 	except IntegrityError:
 		raise Http404("email already in use")
 
+@csrf_exempt
 def login(request):
 	email_in = request.POST.get('email')
 	password_in = request.POST.get('password')
@@ -30,6 +33,7 @@ def login(request):
 		raise Http404("password wrong")
 	return HttpResponse("login successful for " + user.email)
 
+@csrf_exempt
 def fetch_user_trips(request):
 	user_id_in = request.POST.get('user')
 
@@ -37,6 +41,7 @@ def fetch_user_trips(request):
 	success= user.email + " successful fetch_user_trips"
 	return HttpResponse(success)
 
+@csrf_exempt
 def fetch_comm_trips(request, lat_str_in, long_str_in):
 	latitude = float(request.POST.get('lat_str'))
 	longitude = float(request.POST.get('long_str'))

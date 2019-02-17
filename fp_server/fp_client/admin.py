3cd6b7c3af137d4fef1f6ac9e6b7b1efbaec5013
Jing Lin <moderatelyfunctionalprogrammer@gmail>
2019-02-17 07:33:49 -0500

    done

--- fp_server/fp_client/admin.py
+++ fp_server/fp_client/admin.py
@@ -1,8 +1,12 @@
-from .models import Car
+from .models import Cars, Trips
 from django.contrib import admin
 
 # Register your models here.
 class CarAdmin(admin.ModelAdmin):
 	fields = ('name', 'emissions')
 
-admin.site.register(Car, CarAdmin)
+class TripAdmin(admin.ModelAdmin):
+	fields = ('user', 'car_id', 'city', 'end_time', 'dist_traveled', 'dist_walked')
+
+admin.site.register(Cars, CarAdmin)
+admin.site.register(Trips, TripAdmin)
